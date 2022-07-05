19779cdac26746c350bb6c4b35b96bb766959236
mariaIvanova-vn <maria_vn86@abv.bg>
2022-07-05 15:07:34 +0300

    add

--- 01.Inheritance - Exercise/NeedForSpeed/Vehicle.cs
+++ 01.Inheritance - Exercise/NeedForSpeed/Vehicle.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace NeedForSpeed
 {
-    internal class Vehicle
+    public class Vehicle
     {
+        private const double DefaultFuelConsumption = 1.25;
+        private int horsePower;
+        private double fuel;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+
+        public virtual double FuelConsumption
+        {
+            get
+            {
+                return DefaultFuelConsumption;
+            }
+        }
+
+        public virtual void Drive(double kilometers)
+        {
+            Fuel -= FuelConsumption * kilometers;
+        }
     }
 }
