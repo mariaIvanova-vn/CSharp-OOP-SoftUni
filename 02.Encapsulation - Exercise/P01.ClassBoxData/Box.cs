4a353f0aabf94addbaee357bbf5a2050b83469b1
mariaIvanova-vn <maria_vn86@abv.bg>
2022-07-08 10:06:54 +0300

    Encapsulation

---
+++ 02.Encapsulation - Exercise/P01.ClassBoxData/Box.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length,double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length  {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException($"{nameof(this.Length)} cannot be zero or negative.");
+                }
+                this.length = value;
+            } 
+        }
+        public double Width   {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(this.Width)} cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+        public double Height   {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(this.Height)} cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea() => (2 * this.Length * this.Width) + (2 * this.Height * this.Width) + (2 * this.Length * this.Height);
+
+        public double LateralSurfaceArea() => (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+
+        public double Volume() => this.Length * this.Width * this.Height;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Surface Area - {this.SurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {this.LateralSurfaceArea():f2}");
+            sb.Append($"Volume - {this.Volume():f2}");
+
+            return sb.ToString();   
+        }
+    }
+}
