6519311744e61927d668ad28fe38a76898272b3e
mariaIvanova-vn <maria_vn86@abv.bg>
2022-07-12 14:07:25 +0300

    add

---
+++ 03.Interfaces and Abstraction - Exercise/03. Telephony/IO/ConsoleReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Telephony.IO.Interfaces;
+
+namespace Telephony.IO
+{
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            string text = Console.ReadLine();
+            return text;
+        }
+    }
+}
---
+++ 03.Interfaces and Abstraction - Exercise/03. Telephony/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Telephony
+{
+    using Core;
+    using IO;
+    using IO.Interfaces;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IReader reader = new ConsoleReader();
+            IWriter writer = new ConsoleWriter();
+            new Engine(reader, writer).Start();
+        }
+    }
+}
