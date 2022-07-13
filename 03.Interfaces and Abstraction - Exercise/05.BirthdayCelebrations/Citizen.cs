24277c2c0103442d62100f46cfd05b6bdd7a145b
mariaIvanova-vn <maria_vn86@abv.bg>
2022-07-13 10:11:52 +0300

    add

---
+++ 03.Interfaces and Abstraction - Exercise/05.BirthdayCelebrations/Citizen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthdayCelebrations
+{
+    public class Citizen : IIdentifiable, IBirthable
+    {
+        private string name;
+        private int age;
+        private string id;
+        private string birthday;
+        public string Id { get { return id; } set { id = value; } }
+        public string Birthday { get { return birthday; } set { birthday = value; } }
+        public Citizen(string name, int age, string id, string birthday)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthday = birthday;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
