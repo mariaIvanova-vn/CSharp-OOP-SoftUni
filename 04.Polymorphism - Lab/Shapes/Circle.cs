069b445a9f3d6afae8433d6f0b5d5abc0d560491
mariaIvanova-vn <maria_vn86@abv.bg>
2022-07-14 13:25:14 +0300

    add

---
+++ 04.Polymorphism - Lab/Shapes/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; private set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculatePerimeter() 
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * (Radius * Radius);
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() + $"{this.GetType().Name}";
+        }
+    }
+}
---
+++ 04.Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height { get; private set; }
+        public double Width { get; private set; }
+        public override double CalculatePerimeter()
+        {
+            return 2 * Height + 2 * Width;
+        }
+
+        public override double CalculateArea() 
+        {
+            return Height * Width;
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() + $"{this.GetType().Name}";
+        }
+    }
+}
