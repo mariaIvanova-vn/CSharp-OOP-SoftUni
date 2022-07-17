3795d3501aeff1ad4b8c985e7dff211a4aa3c4df
mariaIvanova-vn <maria_vn86@abv.bg>
2022-07-17 18:55:36 +0300

    add

---
+++ 05.Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number=int.Parse(Console.ReadLine());
+            try
+            {
+                if (number<0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                double result = Math.Sqrt(number);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
