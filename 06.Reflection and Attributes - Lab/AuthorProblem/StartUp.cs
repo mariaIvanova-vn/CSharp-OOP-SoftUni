0800171357818dd77382e8f91ec6026e9baba9d9
mariaIvanova-vn <maria_vn86@abv.bg>
2022-07-23 22:56:13 +0300

    Reflection and Attributes

---
+++ 06.Reflection and Attributes - Lab/AuthorProblem/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [AuthorAttribute("Maria")]
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
