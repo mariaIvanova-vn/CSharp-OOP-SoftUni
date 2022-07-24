a2068243303e184e016bfdd635bd0c7c4d5d3d70
mariaIvanova-vn <maria_vn86@abv.bg>
2022-07-24 14:19:37 +0300

    add

---
+++ 06.Reflection and Attributes - Lab/Code Tracker/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [AuthorAttribute("Maria")]
+    public class StartUp
+    {
+        [AuthorAttribute("Maria")]
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ 06.Reflection and Attributes - Lab/Code Tracker/Tracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            foreach (var method in type.GetMethods())
+            {
+                object[] attributes = method.GetCustomAttributes(false);
+                foreach (var item in attributes)
+                {
+                    AuthorAttribute authorAttribute = item as AuthorAttribute;
+                    if (authorAttribute != null)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {authorAttribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
