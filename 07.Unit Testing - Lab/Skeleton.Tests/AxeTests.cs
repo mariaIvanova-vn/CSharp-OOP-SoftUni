a58fb2a9d4b0866edbb8233a443ff2748d30fc6e
mariaIvanova-vn <maria_vn86@abv.bg>
2022-07-27 09:57:25 +0300

    unit testing

--- 07.Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
+++ 07.Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
@@ -6,9 +6,21 @@ namespace Skeleton.Tests
     public class AxeTests
     {
         [Test]
-        public void Test1()
+        public void AxeLoosesDurabilityAfterAttack()
         {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
 
+            axe.Attack(dummy);
+            Assert.AreEqual(9, axe.DurabilityPoints, "Axe durability doesn't change after attack.");
+        }
+        [Test]
+        public void ShouldThrowExceptionIfAxeBroken()
+        {
+            Axe axe = new Axe(10, 0);
+            Dummy dummy = new Dummy(10, 10);
+
+            Assert.That(() => axe.Attack(dummy), Throws.InvalidOperationException.With.Message.EqualTo("Axe is broken."));
         }
     }
 }
