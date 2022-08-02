4c04f2ec4393dfe2c582fa09639e1964f7ee24d0
mariaIvanova-vn <maria_vn86@abv.bg>
2022-08-02 12:53:10 +0300

    add solution for task 1

---
+++ EXAM PREPARATION/C# OOP Exam - 11 December 2021/Gym/Models/Gyms/Gym.cs
@@ -0,0 +1,97 @@
+﻿using Gym.Models.Athletes.Contracts;
+using Gym.Models.Equipment.Contracts;
+using Gym.Models.Gyms.Contracts;
+using Gym.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gym.Models.Gyms
+{
+    public abstract class Gym : IGym
+    {
+        private string name;
+
+        public Gym(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Athletes = new List<IAthlete>();
+            Equipment = new List<IEquipment>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidGymName);
+                }
+                name = value;
+            }
+        }
+
+        public int Capacity { get; }
+
+        public double EquipmentWeight 
+        {
+            get { return Equipment.Select(e => e.Weight).Sum(); }
+        }
+
+        public ICollection<IEquipment> Equipment { get; }
+
+        public ICollection<IAthlete> Athletes { get; }
+
+        public void AddAthlete(IAthlete athlete)
+        {
+            if (Athletes.Count>=Capacity)
+            {
+                throw new InvalidOperationException(ExceptionMessages.NotEnoughSize);
+            }
+            Athletes.Add(athlete);
+        }
+
+        public void AddEquipment(IEquipment equipment)
+        {
+            Equipment.Add(equipment);
+        }
+
+        public void Exercise()
+        {
+            foreach (var item in Athletes)
+            {
+                item.Exercise();
+            }
+        }
+
+        public string GymInfo()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"{Name} is a {GetType().Name}:");
+            string athletes;
+            if (Athletes.Count>0)
+            {
+                athletes = string.Join(", ", Athletes.Select(a => a.FullName));
+            }
+            else
+            {
+                athletes = "No athletes";
+            }
+            stringBuilder.AppendLine($"Athletes: {athletes}");
+            stringBuilder.AppendLine($"Equipment total count: {Equipment.Count}");
+            stringBuilder.Append($"Equipment total weight: {EquipmentWeight:f2} grams");
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        public bool RemoveAthlete(IAthlete athlete)
+        {
+            return Athletes.Remove(athlete);
+        }
+    }
+}
---
+++ EXAM PREPARATION/C# OOP Exam - 11 December 2021/Gym/Repositories/EquipmentRepository.cs
@@ -0,0 +1,31 @@
+﻿using Gym.Models.Equipment;
+using Gym.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Gym.Models.Equipment.Contracts;
+
+namespace Gym.Repositories
+{
+    public class EquipmentRepository : IRepository<IEquipment>
+    {
+        private List<IEquipment> models;
+        public IReadOnlyCollection<IEquipment> Models { get; }
+
+        public void Add(IEquipment model)
+        {
+           models.Add(model);
+        }
+
+        public IEquipment FindByType(string type)
+        {
+            return models.FirstOrDefault(e => e.GetType().Name == type);
+        }
+
+        public bool Remove(IEquipment model)
+        {
+           return models.Remove(model);    
+        }
+    }
+}
