401089cf3720e10a39f8bb9e9182cb4ef9004e55
mariaIvanova-vn <maria_vn86@abv.bg>
2022-08-08 09:37:19 +0300

    add solution for Aquariums.Tests

--- EXAM PREPARATION/C# OOP Exam - 10 April 2021/03. Unit Tests/Aquariums.Tests/AquariumsTests.cs
+++ EXAM PREPARATION/C# OOP Exam - 10 April 2021/03. Unit Tests/Aquariums.Tests/AquariumsTests.cs
@@ -5,46 +5,58 @@
 
     public class AquariumsTests
     {
-        [Test]
-        public void AquariumCreatior()
-        {
-            Aquarium aquarium = new Aquarium("Richie", 3);
+        //[Test]
+        //public void AquariumCreatior()
+        //{
+        //    Aquarium aquarium = new Aquarium("Richie", 3);
 
-            Assert.AreEqual("Richie", aquarium.Name);
-            Assert.AreEqual(0, aquarium.Count);
-            Assert.AreEqual(3, aquarium.Capacity);
-        }
+        //    Assert.AreEqual("Richie", aquarium.Name);
+        //    Assert.AreEqual(0, aquarium.Count);
+        //    Assert.AreEqual(3, aquarium.Capacity);
+        //}
         [Test]
-        public void AquariumThrowWithNullName()
+        public void FishCreatior()
+        {
+            Fish fish = new Fish("Ribcho");
+
+            Assert.AreEqual(fish.Name, "Ribcho");
+            Assert.IsTrue(fish.Available);
+        }     
+        [Test]
+        public void AquariumThrowWithNullAndEmptyName()
         {
             Assert.Throws<ArgumentNullException>(() =>
             {
                 Aquarium aquarium = new Aquarium(null, 5);
             });
-        }
-        [Test]
-        public void AquariumThrowWithEmptyName()
-        {
+
             Assert.Throws<ArgumentNullException>(() =>
             {
-                Aquarium aquarium = new Aquarium("", 5);
+                Aquarium aquarium = new Aquarium(string.Empty, 5);
             });
+        }
+        //}
+        [Test]
+        public void AquariumNamePropertyWorkCorrectly()
+        {
+            Aquarium aquarium = new Aquarium("Richie", 3);
+
+            Assert.That(aquarium.Name, Is.EqualTo("Richie"));
         }
         [Test]
         public void AquariumThrowWithNegativeCapacity()
         {
             Assert.Throws<ArgumentException>(() =>
             {
-                Aquarium aquarium = new Aquarium("fish", -5);
+                Aquarium aquarium = new Aquarium("fish", -1);
             });
         }
         [Test]
-        public void AddFish()
+        public void AquariumWithCorrectCapacity()
         {
             Aquarium aquarium = new Aquarium("Richie", 3);
-            Fish fish = new Fish("Fishi");
-            aquarium.Add(fish);
-            Assert.AreEqual(1, aquarium.Count);
+
+            Assert.That(aquarium.Capacity , Is.EqualTo(3)); 
         }
         [Test]
         public void AddFishThrow()
@@ -56,7 +68,16 @@
             Assert.Throws<InvalidOperationException>(() =>
             {
                 aquarium.Add(fish2);
-            });           
+                Assert.That(aquarium.Capacity, Is.EqualTo(aquarium.Count));
+            });
+        }
+        [Test]
+        public void AddFish()
+        {
+            Aquarium aquarium = new Aquarium("Richie", 3);
+            Fish fish = new Fish("Fishi");
+            aquarium.Add(fish);
+            Assert.That(aquarium.Count, Is.EqualTo(1));
         }
         [Test]
         public void RemoveFish()
@@ -89,22 +110,21 @@
 
             Assert.AreEqual("Fish available at Richie: Fishi", aquarium.Report());
         }
-        //[Test]
-        //public void SellFish()
-        //{
-        //    Aquarium aquarium = new Aquarium("Richie", 3);
-        //    Fish fish = new Fish("Fishi");
-        //    aquarium.Add(fish);
-        //    aquarium.SellFish("Fishi");
-
-        //    Assert.AreEqual(false, aquarium.SellFish("Fishi"));
-        //}
         [Test]
         public void SellFishThrow()
         {
-            Aquarium aquarium = new Aquarium("Richie", 3);           
+            Aquarium aquarium = new Aquarium("Richie", 3);
 
-            Assert.Throws<InvalidOperationException>(() => { aquarium.SellFish(null); });
+            Assert.Throws<InvalidOperationException>(() => { Fish sellFish = aquarium.SellFish(null); });
+        }
+        [Test]
+        public void SellFish()
+        {
+            Aquarium aquarium = new Aquarium("Richie", 3);
+            Fish fish = new Fish("Fishi");
+            aquarium.Add(fish);
+            Fish sellFish =  aquarium.SellFish("Fishi");
+            Assert.IsFalse(sellFish.Available);
         }
     }
 }
